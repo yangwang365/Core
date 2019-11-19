e3e3599d2d0c72dfed7258b3264f425b10ea9f21
Yu Hong <2284428552@qq.com>
2019-11-19 09:58:52 +0800

    Revert "解决MemoryCacheAOP报错问题，添加RedisAOP，目前配置文件报错"
    This reverts commit 603d00a049838003729a6ef51af1f7cdf3853a58.

---
+++ CoreProject.API/AOP/CacheAOP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using CoreProject.Common.CacheHelper;
+
+namespace CoreProject.API.AOP
+{
+    public class CacheAOP : CacheAOPBase
+    {
+        //通过注入的方式，把缓存操作接口通过构造函数注入
+        private readonly ICaching _cache;
+        public CacheAOP(ICaching cache)
+        {
+            _cache = cache;
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            //获取自定义缓存键
+            var cacheKey = CustomCacheKey(invocation);
+            //根据key获取相应的缓存值
+            var cacheValue = _cache.Get(cacheKey);
+            if (cacheValue != null)
+            {
+                //将当前获取到的缓存值，赋值给当前执行方法
+                invocation.ReturnValue = cacheValue;
+                return;
+            }
+            //去执行当前的方法
+            invocation.Proceed();
+            //存入缓存
+            if (!string.IsNullOrWhiteSpace(cacheKey))
+            {
+                _cache.Set(cacheKey, invocation.ReturnValue);
+            }
+        }
+    }
+}
--- CoreProject.API/Program.cs
+++ CoreProject.API/Program.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -15,31 +14,14 @@ namespace CoreProject.API
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();//自动生成
-            var host = CreateHostBuilder(args).Build();
-            using (var scope=host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                try
-                {
-                    var configuration = services.GetRequiredService<IConfiguration>();
-                }
-                catch (Exception e)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError(e, "有错");
-                    throw;
-                }
-            }
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
             .UseServiceProviderFactory(new AutofacServiceProviderFactory())//用于AutoFac，作用把服务和容器添加到项目宿主
                 .ConfigureWebHostDefaults(webBuilder =>
-                { 
+                {
                     webBuilder.UseStartup<Startup>();
                 });
     }
