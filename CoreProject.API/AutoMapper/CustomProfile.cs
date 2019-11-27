e32f8fbd23d20d9e9d5358fab08662adfa055e1b
Yu Hong <2284428552@qq.com>
2019-11-27 17:26:06 +0800

    添加AutoMapper

---
+++ CoreProject.API/AutoMapper/CustomProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CoreProject.API.AutoMapper
+{
+    public class CustomProfile:Profile
+    {
+        public CustomProfile()
+        {
+            //CreateMap<,> ();
+        }
+    }
+}
