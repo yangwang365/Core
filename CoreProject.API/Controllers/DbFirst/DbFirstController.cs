554424cb20251440c5fdfb74e58850edcfbf08aa
Yu Hong <2284428552@qq.com>
2019-11-29 17:27:40 +0800

    发布项目更新

---
+++ CoreProject.API/Controllers/DbFirst/DbFirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreProject.API.Controllers.DbFirst
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    //[Authorize(Permissions.Name)]
+    public class DbFirstController : ControllerBase
+    {
+        private readonly MyContext myContext;
+    }
+}
---
+++ CoreProject.Model/Seed/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SqlSugar;
+using CoreProject.Common.DB;
+
+namespace CoreProject.Model.Seed
+{
+    public class MyContext
+    {
+        private static string _connectionString = BaseDBConfig.ConnectionString;
+        private static DbType _dbType = (DbType)BaseDBConfig.DbType;
+        private SqlSugarClient _db;
+
+
+    }
+}
